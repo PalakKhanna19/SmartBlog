e6b42c99dd60952d1451fc1657565819c405c6a6
palak <khanna@gmail.com>
2017-07-20 17:00:19 +0530

    Separates the keys in a different python file

--- Blog/views.py
+++ Blog/views.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 from django.utils import timezone
 from SmartBlog.settings import BASE_DIR
 from clarifai.rest import ClarifaiApp
+from Blog.keys import YOUR_CLIENT_ID,YOUR_CLIENT_SECRET,SENDGRID_API_KEY
 import json
 import codecs
 
@@ -16,9 +17,6 @@ import sendgrid
 
 from sendgrid.helpers.mail import *
 
-YOUR_CLIENT_ID='b3d706ef096a8de'
-YOUR_CLIENT_SECRET='5100b746cc6c2607712addaf1c3c3fb0d96a0f0f'
-SENDGRID_API_KEY='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'
 
 
 
