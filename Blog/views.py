0ec1bf34966a35c6fe1a4558ab69c40c0e72ccaa
palak <khanna@gmail.com>
2017-07-23 01:03:46 +0530

    Added the functionality of Logout by creating Logout_view and updating feed.html and urls.py

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
-from models import UserModel, SessionToken, PostModel, LikeModel, CommentModel
+from models import UserModel, SessionToken, PostModel, LikeModel, CommentModel,CategoryModel
 from django.contrib.auth.hashers import make_password, check_password
 from datetime import timedelta
 from django.utils import timezone
@@ -52,7 +52,7 @@ def signup_view(request):
     else:
         form = SignUpForm()
 
-    return render(request, 'index.html', {'form': form})
+    return render(request, 'index.html' , {'form': form})
 
 
 def login_view(request):
@@ -133,9 +133,8 @@ def add_category(post):
         if response["outputs"]:
             if response["output"][0]["data"]:
                 if response["output"][0]["data"]["concepts"]:
-                    for index in range (0,len(response)["outputs"][0]["data"]["concepts"]):
-                        category=Category.Models(post=post,category_text=response['outputs'][0]['data']['concepts'][index]['name'])
-                        print response
+                    for index in range (0,len(response["outputs"][0]["data"]["concepts"])):
+                        category=CategoryModel(post=post,category_text=response['outputs'][0]['data']['concepts'][index]['name'])
                         category.save()
                 else:
                     print 'no concepts error'
@@ -173,21 +172,22 @@ def like_view(request):
             post_id = form.cleaned_data.get('post').id
 
             existing_like = LikeModel.objects.filter(post_id=post_id, user=user).first()
-            like = LikeModel.objects.create(post_id=post_id, user=user)
-            sg = sendgrid.SendGridAPIClient(apikey=(SENDGRID_API_KEY))
-            from_email = Email("khannapalak19@gmail.com")
-            to_email = Email(like.post.user.email)
-            subject = "Welcome to Smartblog"
-            content = Content("text/plain",
-                              "Team Smartblog welcomes you!\n We hope you enjoy sharing your precious moments blogging them /n")
-            mail = Mail(from_email, subject, to_email, content)
-            response = sg.client.mail.send.post(request_body=mail.get())
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
+
+
 
             if not existing_like:
-                LikeModel.objects.create(post_id=post_id, user=user)
+                like=LikeModel.objects.create(post_id=post_id, user=user)
+                sg = sendgrid.SendGridAPIClient(apikey=(SENDGRID_API_KEY))
+                from_email = Email("khannapalak19@gmail.com")
+                to_email = Email(like.post.user.email)
+                subject = "You have a new like on your post %d " % (post_id)
+                content = Content("text/plain",
+                                  "You have a new like on your post %d /n Login to view the details" % post_id)
+                mail = Mail(from_email, subject, to_email, content)
+                response = sg.client.mail.send.post(request_body=mail.get())
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
             else:
                 existing_like.delete()
             return redirect('/feed/')
@@ -227,8 +227,14 @@ def check_validation(request):
     if request.COOKIES.get('session_token'):
         session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
         if session:
-            time_to_live = session.created_on + timedelta(days=1)
+             time_to_live = session.created_on + timedelta(days=1)
             if time_to_live > timezone.now():
                 return session.user
     else:
         return None
+
+def logout_view(request):
+    request.session.modified= True
+    response=redirect('/login/')
+    response.delete_cookie(key="session_token")
+    return response
--- SmartBlog/urls.py
+++ SmartBlog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import url
-from Blog.views import signup_view,login_view,post_view,feed_view,like_view,comment_view
+from Blog.views import signup_view,login_view,post_view,feed_view,like_view,comment_view,logout_view
 
 urlpatterns = [
+    url('/login/', logout_view),
     url('post/', post_view),
     url('feed/', feed_view),
     url('like/', like_view),
