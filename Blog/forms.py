256f7b8f3e6b6e337bbc4b18526d0223647b52c0
palak <khanna@gmail.com>
2017-07-24 21:42:02 +0530

    Added comments to form.py

--- Blog/forms.py
+++ Blog/forms.py
@@ -1,30 +1,32 @@
 from django import forms
 from models import UserModel, PostModel, LikeModel, CommentModel
 
-
+#created form for signup
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = UserModel
         fields=['email','username','name','password']
 
+#created form for login
 class LoginForm(forms.ModelForm):
     class Meta:
         model = UserModel
         fields = ['username', 'password']
 
+#created form for post
 class PostForm(forms.ModelForm):
     class Meta:
         model = PostModel
         fields=['image', 'caption']
 
-
+#created form for Like
 class LikeForm(forms.ModelForm):
 
     class Meta:
         model = LikeModel
         fields=['post']
 
-
+#created form for comment
 class CommentForm(forms.ModelForm):
 
     class Meta:
